b2add1caa04cdba796876f01fcc5b8b18d6e96fb
Roman <r.jouhannet@f5.com>
2018-08-03 17:37:07 -0700

    update scripts UDF

--- lab/update_git.sh
+++ lab/update_git.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd /home/f5
-git clone https://github.com/f5devcentral/f5-big-iq-lab.git
+rm -rf AWS* bigip* f5-ansi* scripts* class1* Common* crontab* f5-big-iq-lab
+#git clone https://github.com/f5devcentral/f5-big-iq-lab.git --branch master
+git clone https://github.com/f5devcentral/f5-big-iq-lab.git --branch develop
 mv /home/f5/f5-big-iq-lab/lab/* /home/f5
-rm -f /home/f5/f5-big-iq-lab
+rm -rf /home/f5/f5-big-iq-lab
+chmod +x *py scripts/*sh scripts/*py f5-ansible-demo/*sh *sh AWS*/*sh AWS*/*py
