8ec5a4305ac6a3f5b96483fc0344fe0f1b17ffdb
Roman <r.jouhannet@f5.com>
2018-08-09 20:50:00 -0700

    Update scripts

--- lab/update_git.sh
+++ lab/update_git.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 cd /home/f5
-rm -rf AWS* bigip* f5-ansi* scripts* class1* Common* crontab* f5-big-iq-lab
+echo "Cleanup previous scripts..."
+rm -rf AWS* f5-ansi* scripts* class1* Common* crontab* f5-big-iq-lab
+echo "Install new scripts..."
 #git clone https://github.com/f5devcentral/f5-big-iq-lab.git --branch master
 git clone https://github.com/f5devcentral/f5-big-iq-lab.git --branch develop
 mv /home/f5/f5-big-iq-lab/lab/* /home/f5
 rm -rf /home/f5/f5-big-iq-lab
+echo "Fixing permissions..."
 chmod +x *py scripts/*sh scripts/*py scripts/access/*sh scripts/access/*py f5-ansible-demo/*sh *sh AWS*/*sh AWS*/*py
 
 # for SCJ - DCD IP
