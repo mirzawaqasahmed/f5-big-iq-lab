8b1da1b14846c37d2a912fa348e1ff15c1c7c2fb
Roman <r.jouhannet@f5.com>
2018-08-14 13:26:03 -0700

    Update scripts

--- lab/update_git.sh
+++ lab/update_git.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+## Configured in /etc/rc.local
+## /home/f5student/update_git.sh
+## chown -R f5student:f5student /home/f5student
+
 cd /home/f5
 
 if [ -f ~/udf_auto_update_git ]; then
     echo -e "\nIn order to force the scripts/tools updates, delete udf_auto_update_git and re-run update_git.sh (optional).\n"
 else
     echo "Cleanup previous scripts..."
-    rm -rf AWS* f5-ansi* scripts* class1* Common* crontab* f5-big-iq-lab
+    rm -rf AWS* f5-ansi* scripts* class1* Common* crontab* f5-big-iq-lab vmware-ansible
     echo "Install new scripts..."
     #git clone https://github.com/f5devcentral/f5-big-iq-lab.git --branch master
     git clone https://github.com/f5devcentral/f5-big-iq-lab.git --branch develop
@@ -17,7 +21,7 @@ else
     rm -rf AWS-CFT-Cloud-Edition-6.0.0
     mv AWS-CFT-Cloud-Edition-6.0.1 AWS-CFT-Cloud-Edition
     echo "Fixing permissions..."
-    chmod +x *py scripts/*sh scripts/*py scripts/access/*sh scripts/fps/*py f5-ansible-demo/*sh *sh AWS*/*sh AWS*/*py
+    chmod +x *py scripts/*sh scripts/*py scripts/access/*sh scripts/fps/*py f5-ansible-demo/*sh *sh AWS*/*sh AWS*/*py vmware-ansible/*sh
     echo "Installing new crontab"
     crontab < crontab.txt
     # Cleanup AWS credentials
