7dedb66f53624998f79c284153eeb5551dc0e3c3
Roman <r.jouhannet@f5.com>
2018-08-21 12:32:10 -0700

    update_git update

--- lab/update_git.sh
+++ lab/update_git.sh
@@ -33,10 +33,8 @@ else
     # for SEA - DCD IP
     #sed -i 's/10.1.10.6/10.11.150.16/g' /home/f5/scripts/*sh
 
-    ## WA docker hackazon processes not starting sometimes (fix to re-install the docker => next publication)
-    sudo docker restart 2e4fc9adf5be
     ## WA restart xrdp as sometime the process dies
-    sudo /etc/init.d/xrdp restart
+    #sudo /etc/init.d/xrdp restart
 
     # Temporart package to install for vmware script
     sudo apt-get install jq -y
@@ -45,6 +43,9 @@ else
     rm -f last_update_*
     touch last_update_$(date +%Y-%m-%d_%H-%M)
 fi
+
+## WA docker hackazon processes not starting sometimes (fix to re-install the docker => next publication)
+sudo docker restart 2e4fc9adf5be
 
 # Restart VM in case any are powered off (for VMware SSG if deployment was shutdown)
 /home/f5student/vmware-ansible/cmd_power_on_vm.sh > /home/f5student/vmware-ansible/cmd_power_on_vm.log &
