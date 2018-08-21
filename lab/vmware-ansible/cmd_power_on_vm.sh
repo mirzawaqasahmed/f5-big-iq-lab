d13b78018a1d3ad8518ac9b0ebc2ac190997e9c7
Roman <r.jouhannet@f5.com>
2018-08-21 00:10:03 -0700

    vmware ansible

--- lab/vmware-ansible/cmd_power_on_vm.sh
+++ lab/vmware-ansible/cmd_power_on_vm.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
+# Uncomment set command below for code debuging bash
+#set -x
+
+cd /home/f5/vmware-ansible
 
 ansible-playbook -i notahost, facts_vm.yaml
 
+jq '.virtual_machines' vmfact.json | grep uuid | awk -F: '{print $2}' | sed -n 's/^.*"\(.*\)".*$/\1/p' > uuid.txt
 
-# ansible-playbook -i notahost, power_on_vm.yaml -vvvv
+while IFS= read -r uuid
+do
+  echo "$uuid"
+  ansible-playbook -i inventory_vm.ini, power_on_vm.yaml --extra-vars "uuid=$uuid" -vvvv
+done < uuid.txt
+
+rm -f vmfact.json uuid.txt
