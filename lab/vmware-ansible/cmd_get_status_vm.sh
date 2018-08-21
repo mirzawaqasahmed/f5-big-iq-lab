614629ebad647f0765657e53f58d99b7690c2a61
Roman <r.jouhannet@f5.com>
2018-08-21 05:13:21 -0700

    vmware ansible

--- lab/vmware-ansible/cmd_get_status_vm.sh
+++ lab/vmware-ansible/cmd_get_status_vm.sh
@@ -6,18 +6,6 @@ cd /home/f5/vmware-ansible
 
 ansible-playbook -i notahost, get_status_vm.yaml
 
-jq '.virtual_machines' vmfact.json | grep uuid | awk -F: '{print $2}' | sed -n 's/^.*"\(.*\)".*$/\1/p' > uuid.txt
+jq '.virtual_machines' vmfact.json
 
-n=$(wc -l uuid.txt | awk '{ print $1 }')
-
-if [ "$n" -gt "1" ]; then
-    while IFS= read -r uuid
-    do
-        echo -e "\n#### $uuid"
-        ansible-playbook -i notahost, power_on_vm.yaml --extra-vars "uuid=$uuid"
-    done < uuid.txt
-else
-    echo "No SSG VM to power on."
-fi
-
-rm -f vmfact.json uuid.txt
+rm -f vmfact.json
--- lab/vmware-ansible/cmd_power_on_vm.sh
+++ lab/vmware-ansible/cmd_power_on_vm.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 # Uncomment set command below for code debuging bash
 #set -x
@@ -6,4 +7,18 @@ cd /home/f5/vmware-ansible
 
 ansible-playbook -i notahost, get_status_vm.yaml
 
-jq '.virtual_machines' vmfact.json
+jq '.virtual_machines' vmfact.json | grep uuid | awk -F: '{print $2}' | sed -n 's/^.*"\(.*\)".*$/\1/p' > uuid.txt
+
+n=$(wc -l uuid.txt | awk '{ print $1 }')
+
+if [ "$n" -gt "1" ]; then
+    while IFS= read -r uuid
+    do
+        echo -e "\n#### $uuid"
+        ansible-playbook -i notahost, power_on_vm.yaml --extra-vars "uuid=$uuid"
+    done < uuid.txt
+else
+    echo "No SSG VM to power on."
+fi
+
+rm -f vmfact.json uuid.txt
