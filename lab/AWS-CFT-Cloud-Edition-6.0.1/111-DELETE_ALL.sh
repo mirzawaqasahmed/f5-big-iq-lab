bbee37123f7c50caa3af41e6fc5bb806ddfb4d32
Roman <r.jouhannet@f5.com>
2018-09-12 09:56:48 -0700

    update 9/12

--- lab/AWS-CFT-Cloud-Edition-6.0.1/111-DELETE_ALL.sh
+++ lab/AWS-CFT-Cloud-Edition-6.0.1/111-DELETE_ALL.sh
@@ -4,10 +4,15 @@
 # Uncomment set command below for code debuging ansible
 #DEBUG_arg="-vvvv"
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 function pause(){
    read -p "$*"
 }
-echo -e "\n\n/!\ DELETION OF ALL AWS OBJECTS (Application/SSG/VPN/VPC) /!\ \n"
+echo -e "\n\n${RED}/!\ DELETION OF ALL AWS OBJECTS (Application/SSG/VPN/VPC) /!\ ${NC} \n"
 
 [[ $1 != "nopause" ]] && pause 'Press [Enter] key to continue... CTRL+C to Cancel'
 
@@ -15,37 +20,37 @@ clear
 
 echo -e "\n\nEXPECTED TIME: ~25 min\n\n"
 
-echo -e "TIME: $(date +"%H:%M")"
+echo -e "${BLUE}TIME: $(date +"%H:%M")${NC}"
 ansible-playbook $DEBUG_arg -i notahost, 10-delete-aws-waf-app.yml
-echo -e "\nTIME: $(date +"%H:%M")"
+echo -e "\n${BLUE}TIME: $(date +"%H:%M")${NC}"
 
-echo -e "\n\n/!\ HAVE YOU DELETED THE APP CREATED ON YOUR SSG FROM BIG-IQ? /!\ \n"
-echo -e "IF YOU HAVE NOT, PLEASE DELETE ANY APPLICATION(S) CREATED ON YOUR AWS SSG BEFORE PROCEEDING\n\n"
+echo -e "\n\n${RED}/!\ HAVE YOU DELETED THE APP CREATED ON YOUR SSG FROM BIG-IQ? /!\ \n"
+echo -e "IF YOU HAVE NOT, PLEASE DELETE ANY APPLICATION(S) CREATED ON YOUR AWS SSG BEFORE PROCEEDING ${NC}\n\n"
 
 [[ $1 != "nopause" ]] && pause 'Press [Enter] key to continue... CTRL+C to Cancel'
 
-echo -e "\nTIME: $(date +"%H:%M")"
+echo -e "\n${BLUE}TIME: $(date +"%H:%M")${NC}"
 ansible-playbook $DEBUG_arg 11-delete-aws-ssg-resources.yml -i ansible2.cfg
-echo -e "\nTIME: $(date +"%H:%M")"
+echo -e "\n${BLUE}TIME: $(date +"%H:%M")${NC}"
 
-echo -e "\nTIME: $(date +"%H:%M")"
+echo -e "\n${BLUE}TIME: $(date +"%H:%M")${NC}"
 python 11-delete-aws-ssg-resources-check.py
-echo -e "\nTIME: $(date +"%H:%M")"
+echo -e "\n${BLUE}TIME: $(date +"%H:%M")${NC}"
 
 [[ $1 != "nopause" ]] && pause 'Press [Enter] key to continue... CTRL+C to Cancel'
 
-echo -e "/!\ IS YOUR SSG COMPLETLY REMOVED FROM YOUR AWS ACCOUNT? /!\ \n"
-echo -e "MAKE SURE THE AWS SSG HAS BEEN REMOVED COMPLETLY BEFORE PROCEEDING\n"
+echo -e "${RED}/!\ IS YOUR SSG COMPLETLY REMOVED FROM YOUR AWS ACCOUNT? /!\ \n"
+echo -e "MAKE SURE THE AWS SSG HAS BEEN REMOVED COMPLETLY BEFORE PROCEEDING${NC}\n"
 
 [[ $1 != "nopause" ]] && pause 'Press [Enter] key to continue... CTRL+X to Cancel'
 
-echo -e "\nTIME: $(date +"%H:%M")"
+echo -e "\n${BLUE}TIME: $(date +"%H:%M")${NC}"
 ansible-playbook $DEBUG_arg 12-teardown-aws-vpn-vpc-ubuntu.yml -i ansible2.cfg
-echo -e "\nTIME: $(date +"%H:%M")"
+echo -e "\n${BLUE}TIME: $(date +"%H:%M")${NC}"
 
 echo -e "Clear cache directory and *retry"
 rm -rf cache *.retry
 
-echo -e "\n/!\ DOUBLE CHECK IN YOUR AWS ACCOUNT ALL THE RESOURCES CREATED FOR YOUR DEMO HAVE BEEN DELETED  /!\ \n"
+echo -e "\n${RED}/!\ DOUBLE CHECK IN YOUR AWS ACCOUNT ALL THE RESOURCES CREATED FOR YOUR DEMO HAVE BEEN DELETED  /!\ ${NC}\n"
 
 exit 0
