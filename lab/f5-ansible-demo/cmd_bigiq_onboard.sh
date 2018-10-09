2233bc5d94610d2aa80a9689419d64857a9b10e1
Roman Jouhannet <r.jouhannet@f5.com>
2018-10-09 13:44:21 -0700

    10/9

--- lab/f5-ansible-demo/cmd_bigiq_onboard.sh
+++ lab/f5-ansible-demo/cmd_bigiq_onboard.sh
@@ -4,8 +4,11 @@
 # Uncomment set command below for code debugging ansible
 #DEBUG_arg="-vvvv"
 
-ip_cm1="$(cat bigiq_onboard_var/bigiq-cm-01.yml| grep bigiq_onboard_server | awk '{print $2}')"
-ip_dcd1="$(cat bigiq_onboard_var/bigiq-dcd-01.yml| grep bigiq_onboard_server | awk '{print $2}')"
+env="udf"
+#env="sjc2"
+
+ip_cm1="$(cat inventory/group_vars/$env-bigiq-cm-01.yml| grep bigiq_onboard_server | awk '{print $2}')"
+ip_dcd1="$(cat inventory/group_vars/$env-bigiq-dcd-01.yml| grep bigiq_onboard_server | awk '{print $2}')"
 
 # basic auth needs to be turn on on BIG-IQ.
 ssh root@$ip_cm1 set-basic-auth on
@@ -13,7 +16,7 @@ ssh root@$ip_dcd1 set-basic-auth on
 
 ansible-galaxy install f5devcentral.bigiq_onboard --force
 
-ansible-playbook -i bigiq_onboard_var/hosts bigiq_onboard.yaml $DEBUG_arg
+ansible-playbook -i inventory/$env-hosts bigiq_onboard.yaml $DEBUG_arg
 
 # SJC lab 2
-# ansible-playbook -i bigiq_onboard_var/hosts .bigiq_onboard_sjc2.yaml $DEBUG_arg
+# ansible-playbook -i inventory/$env-hosts .bigiq_onboard_sjc2.yaml $DEBUG_arg
--- lab/f5-ansible-demo/cmd_bigiq_onboard_reset.sh
+++ lab/f5-ansible-demo/cmd_bigiq_onboard_reset.sh
@@ -2,8 +2,11 @@
 # Uncomment set command below for code debugging bash
 #set -x
 
-ip_cm1="$(cat bigiq_onboard_var/bigiq-cm-01.yml| grep bigiq_onboard_server | awk '{print $2}')"
-ip_dcd1="$(cat bigiq_onboard_var/bigiq-dcd-01.yml| grep bigiq_onboard_server | awk '{print $2}')"
+env="udf"
+#env="sjc2"
+
+ip_cm1="$(cat inventory/group_vars/$env-bigiq-cm-01.yml| grep bigiq_onboard_server | awk '{print $2}')"
+ip_dcd1="$(cat inventory/group_vars/$env-bigiq-dcd-01.yml| grep bigiq_onboard_server | awk '{print $2}')"
 
 # basic auth needs to be turn on on BIG-IQ.
 ssh root@$ip_cm1 clear-rest-storage -d
