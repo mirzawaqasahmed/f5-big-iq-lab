6bb0379eafd0259b7c0de69163c0f47a174e265f
Romain Jouhannet <r.jouhannet@f5.com>
2018-10-09 16:27:01 -0700

    10/9

--- lab/f5-ansible-demo/cmd_bigiq_onboard.sh
+++ lab/f5-ansible-demo/cmd_bigiq_onboard.sh
@@ -20,3 +20,24 @@ ansible-playbook -i inventory/$env-hosts bigiq_onboard.yaml $DEBUG_arg
 
 # SJC lab 2
 # ansible-playbook -i inventory/$env-hosts .bigiq_onboard_sjc2.yaml $DEBUG_arg
+
+# Add DCD to CM
+curl https://s3.amazonaws.com/big-iq-quickstart-cf-templates-aws/6.0.1.1/scripts.tar.gz > scripts.tar.gz
+rm -rf scripts 
+tar --strip-components=1 -xPvzf scripts.tar.gz 2> /dev/null &
+
+echo "Type BIG-IQ root password:"
+ssh-copy-id root@$ip_cm1
+
+scp -rp scripts root@$ip_cm1:/root
+ssh root@$ip_cm1 << EOF
+  cd /root/scripts
+  /usr/local/bin/python2.7 ./add-dcd.py --DCD_IP_ADDRESS $ip_dcd1
+  sleep 5
+  /usr/local/bin/python2.7 ./activate-dcd-services.py --DCD_IP_ADDRESS $ip_dcd1 --LIST_SERVICES asm
+  /usr/local/bin/python2.7 ./activate-dcd-services.py --DCD_IP_ADDRESS $ip_dcd1 --LIST_SERVICES access
+  /usr/local/bin/python2.7 ./activate-dcd-services.py --DCD_IP_ADDRESS $ip_dcd1 --LIST_SERVICES dos
+  /usr/local/bin/python2.7 ./activate-dcd-services.py --DCD_IP_ADDRESS $ip_dcd1 --LIST_SERVICES websafe
+  /usr/local/bin/python2.7 ./activate-dcd-services.py --DCD_IP_ADDRESS $ip_dcd1 --LIST_SERVICES ipsec
+  /usr/local/bin/python2.7 ./activate-dcd-services.py --DCD_IP_ADDRESS $ip_dcd1 --LIST_SERVICES afm
+EOF
