51aceb48f6953fd24aa5e1de022b59a8574b5ff1
Roman <r.jouhannet@f5.com>
2018-12-11 18:11:43 -0800

    update 12/11

--- lab/f5-ansible-bigiq-as3-demo/cmd_all_playbooks.sh
+++ lab/f5-ansible-bigiq-as3-demo/cmd_all_playbooks.sh
@@ -50,49 +50,49 @@ echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
 
 ansible-playbook -i inventory/$env-hosts as3_bigiq_task01_create_http_app.yml $DEBUG_arg --extra-vars "env=$env_playbook user=$user_playbook"
 echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
-sleep 10
+sleep 15
 
 [[ $1 != "nopause" ]] && pause "Press [Enter] key to continue... CTRL+C to Cancel"
 ansible-playbook -i inventory/$env-hosts as3_bigiq_task02_create_https_app.yml $DEBUG_arg --extra-vars "env=$env_playbook user=$user_playbook"
 echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
-sleep 10
+sleep 15
 
 [[ $1 != "nopause" ]] && pause "Press [Enter] key to continue... CTRL+C to Cancel"
 ansible-playbook -i inventory/$env-hosts as3_bigiq_task03a_create_waf_app.yml $DEBUG_arg --extra-vars "env=$env_playbook user=$user_playbook"
 echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
-sleep 10
+sleep 15
 
 [[ $1 != "nopause" ]] && pause "Press [Enter] key to continue... CTRL+C to Cancel"
 ansible-playbook -i inventory/$env-hosts as3_bigiq_task03b_create_waf_ext_policy_app.yml $DEBUG_arg --extra-vars "env=$env_playbook user=$user_playbook"
 echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
-sleep 10
+sleep 15
 
 [[ $1 != "nopause" ]] && pause "Press [Enter] key to continue... CTRL+C to Cancel"
 ansible-playbook -i inventory/$env-hosts as3_bigiq_task04_create_generic_app.yml $DEBUG_arg --extra-vars "env=$env_playbook user=$user_playbook"
 echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
-sleep 10
+sleep 15
 
 [[ $1 != "nopause" ]] && pause "Press [Enter] key to continue... CTRL+C to Cancel"
 ansible-playbook -i inventory/$env-hosts as3_bigiq_task05a_modify_post_http_app.yml $DEBUG_arg --extra-vars "env=$env_playbook user=$user_playbook"
 echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
-sleep 10
+sleep 15
 
 [[ $1 != "nopause" ]] && pause "Press [Enter] key to continue... CTRL+C to Cancel"
 ansible-playbook -i inventory/$env-hosts as3_bigiq_task05b_modify_patch_http_app.yml $DEBUG_arg --extra-vars "env=$env_playbook user=$user_playbook"
 echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
-sleep 10
+sleep 15
 
 [[ $1 != "nopause" ]] && pause "Press [Enter] key to continue... CTRL+C to Cancel"
 # Need to use an admin user to create the template
-ansible-playbook -i inventory/$env-hosts as3_bigiq_task06_create_template.yml $DEBUG_arg --extra-vars "env=$env_playbook user=david"
+ansible-playbook -i inventory/$env-hosts as3_bigiq_task06_create_template.yml $DEBUG_arg --extra-vars "env=$env_playbook user=$user_playbook"
 echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
-sleep 10
+sleep 15
 
 echo -e "\n${RED}Warning${NC}: Follow Task 8 from the lab guide:\n Assign ${BLUE}HTTPcustomTemplateTask6${NC} template to Applicator Creator AS3 custom role and remove the ${BLUE}default${NC} template from the allowed list).\n"
 pause "Press [Enter] key to continue... CTRL+C to Cancel"
-ansible-playbook -i inventory/$env-hosts as3_bigiq_task08_create_http_app.yml $DEBUG_arg --extra-vars "env=$env_playbook user=$user_playbook"
+ansible-playbook -i inventory/$env-hosts as3_bigiq_task08_create_http_app.yml $DEBUG_arg --extra-vars "env=$env_playbook user=olivia"
 echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
-sleep 10
+sleep 15
 
 echo -e "\n${BLUE}TIME:: $(date +"%H:%M")${NC}"
 [[ $1 != "nopause" ]] && pause "Press [Enter] key to continue... CTRL+C to Cancel"
